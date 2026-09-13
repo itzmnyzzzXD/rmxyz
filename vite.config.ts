391970086baf32d68b7eb421edd7899f204dd6fd
itzmnyzzzXD <mua35351@gmail.com>
2026-09-13 12:19:22 +0300

    Fix Tailwind Vite integration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tanstackStart(), nitro(), viteReact()],
+  plugins: [tanstackStart(), tailwindcss(), nitro(), viteReact()],
 });
